2e4a059771e94f6c50ee0519aca21476913b2be2
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-09-16 06:02:24 +0900

    [FIX] 알림 조회 시 공지 타입 영어, 한글 동시 조회 가능

--- src/main/java/depth/mvp/thinkerbell/domain/user/dto/AlarmDto.java
+++ src/main/java/depth/mvp/thinkerbell/domain/user/dto/AlarmDto.java
@@ -10,7 +10,8 @@ public class AlarmDto {
 
     private Long id;
     private String title;
-    private String noticeType;
+    private String noticeTypeKorean;
+    private String noticeTypeEnglish;
     private boolean isViewed;
     private boolean isMarked;
     private String Url;
