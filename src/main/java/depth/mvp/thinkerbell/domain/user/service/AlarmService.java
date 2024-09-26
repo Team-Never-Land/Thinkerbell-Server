01e060d183143dd79d92985626649680247f9044
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-09-27 01:36:12 +0900

    [FIX] 비동기 처리 시 데이터 업데이트 로직 동시성 처리

--- src/main/java/depth/mvp/thinkerbell/domain/user/service/AlarmService.java
+++ src/main/java/depth/mvp/thinkerbell/domain/user/service/AlarmService.java
@@ -89,28 +89,24 @@ public class AlarmService {
                 }
             }
 
-            //크롤링번호 최신화
-            try {
-                Long newMaxID = allNoticeViewRepository.findMaxIdByCategory(crawlingNum.getNoticeType());
+            Long newMaxID = allNoticeViewRepository.findMaxIdByCategory(crawlingNum.getNoticeType());
 
-                Optional<CrawlingNum> existingCrawlingNumOpt = crawlingNumRepository.findByNoticeType(crawlingNum.getNoticeType());
+            updateCrawlingNum(crawlingNum, newMaxID);
+        }
+    }
 
-                if (existingCrawlingNumOpt.isPresent()) {
-                    // 존재하는 경우 업데이트
-                    CrawlingNum existingCrawlingNum = existingCrawlingNumOpt.get();
-                    existingCrawlingNum.setNoticeID(newMaxID);
-                    crawlingNumRepository.save(existingCrawlingNum);
-                } else {
-                    // 존재하지 않는 경우 새로 저장
-                    CrawlingNum newCrawlingNum = CrawlingNum.builder()
-                            .noticeID(newMaxID)
-                            .noticeType(crawlingNum.getNoticeType())
-                            .build();
-                    crawlingNumRepository.save(newCrawlingNum);
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(crawlingNum.getNoticeType() + "의 크롤링 번호를 업데이트 하는 도중 오류가 발생했습니다.", e);
-            }
+    public synchronized void updateCrawlingNum(CrawlingNum crawlingNum, Long newMaxID) {
+        Optional<CrawlingNum> existingCrawlingNumOpt = crawlingNumRepository.findByNoticeType(crawlingNum.getNoticeType());
+        if (existingCrawlingNumOpt.isPresent()) {
+            CrawlingNum existingCrawlingNum = existingCrawlingNumOpt.get();
+            existingCrawlingNum.setNoticeID(newMaxID);
+            crawlingNumRepository.save(existingCrawlingNum);
+        } else {
+            CrawlingNum newCrawlingNum = CrawlingNum.builder()
+                    .noticeID(newMaxID)
+                    .noticeType(crawlingNum.getNoticeType())
+                    .build();
+            crawlingNumRepository.save(newCrawlingNum);
         }
     }
 
