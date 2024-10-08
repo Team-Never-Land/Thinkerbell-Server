8525828f2fe5e086bc9f7b1c8f44cb575b714610
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-10-09 01:16:51 +0900

    [FEATURE] Pascal To Snake 로직 추가

--- src/main/java/depth/mvp/thinkerbell/domain/common/service/CategoryService.java
+++ src/main/java/depth/mvp/thinkerbell/domain/common/service/CategoryService.java
@@ -40,6 +40,28 @@ public class CategoryService {
         }
     }
 
+    public String convertPascalToSnake(String pascalStr) {
+        StringBuilder snakeStr = new StringBuilder();
+
+        for (int i = 0; i < pascalStr.length(); i++) {
+            char currentChar = pascalStr.charAt(i);
+            // 대문자인 경우
+            if (Character.isUpperCase(currentChar)) {
+                // 첫 번째 문자가 아니면 '_'를 추가
+                if (snakeStr.length() > 0) {
+                    snakeStr.append('_');
+                }
+                // 대문자를 소문자로 변환하여 추가
+                snakeStr.append(Character.toLowerCase(currentChar));
+            } else {
+                // 소문자 문자는 그대로 추가
+                snakeStr.append(currentChar);
+            }
+        }
+
+        return snakeStr.toString();
+    }
+
     public String convertEnglishToKorea(String category) {
         return categoryMap1.getOrDefault(category, category);
     }
